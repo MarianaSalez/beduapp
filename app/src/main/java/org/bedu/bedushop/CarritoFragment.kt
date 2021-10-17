2a04bc542ae5aef1d5fdb15f589322782ff1b10d
MarianaSalez <mariana.i.salez@gmail.com>
2021-10-17 13:56:23 -0300

    Detail envia info. Falta solucionar como la escucha el carrito Fragment

--- app/src/main/java/org/bedu/bedushop/CarritoFragment.kt
+++ app/src/main/java/org/bedu/bedushop/CarritoFragment.kt
@@ -1,6 +1,7 @@
 package org.bedu.bedushop
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.AttributeSet
 import androidx.fragment.app.Fragment
@@ -8,20 +9,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.fragment.app.FragmentManager
+import com.squareup.picasso.Picasso
+import io.realm.Realm
 
 
 class CarritoFragment : Fragment() {
 
 
+    private lateinit var mpName: TextView
+    private lateinit var mpimg: ImageView
+    private lateinit var mpPrice: TextView
+    private lateinit var products: List<Product>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
+        // Inflate the layout for this fragment
         val resumen = ResumenPagoFragment()
-        val view =  inflater.inflate(R.layout.fragment_carrito, container, false)
+        val view = inflater.inflate(R.layout.fragment_carrito, container, false)
+
+
+        //Me traigo lo visual
+        mpName = view.findViewById(R.id.titleCarrito)
+        mpimg = view.findViewById(R.id.carritoImageView)
+        mpPrice = view.findViewById(R.id.priceCarrito)
+
+        //Funcion para pasar al fragment de compra
         val boton = view.findViewById<Button>(R.id.buttonComprar)
         boton.setOnClickListener {
             replaceFragment(resumen, null)
@@ -29,7 +48,8 @@ class CarritoFragment : Fragment() {
 
         return view
     }
-    private fun replaceFragment(fragment: Fragment, bundle:Bundle?){
+
+    private fun replaceFragment(fragment: Fragment, bundle: Bundle?) {
         fragment.arguments = bundle//Enviamos Bundle, de existir
         val trans = requireActivity().supportFragmentManager.beginTransaction()
         trans.replace(R.id.fragemento_contenedor, fragment)
@@ -37,4 +57,33 @@ class CarritoFragment : Fragment() {
         trans.commit()
     }
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val realm = Realm.getDefaultInstance()
+        products = realm.where(Product::class.java).findAll()
+
+        val id = intent.getIntExtra("id", 0)
+        var product = products[id].id
+        val marketObject =
+            supportFragmentManager.findFragmentById(R.id.recyclerCarrito) as? CarritoFragment
+        if (product != null) {
+            marketObject?.showInMarket(product, products)
+
+
+        }
+    }
+
+
+        //funcion en proceso para linkear elementos on lo visual
+
+        fun showInMarket(id: Int, product: List<Product>) {
+            val producto=product[id]
+            view?.visibility = View.VISIBLE
+            mpName.text = producto.name
+           Picasso.with(requireContext()).load(producto.idImage).into(mpimg)
+           mpPrice.text = "$${producto.price}"
+
+        }
+    }
 }
--- app/src/main/java/org/bedu/bedushop/DetailActivity.kt
+++ app/src/main/java/org/bedu/bedushop/DetailActivity.kt
@@ -41,38 +41,34 @@ class DetailActivity : AppCompatActivity() {
             excludeTarget(android.R.id.statusBarBackground, true)
             excludeTarget(android.R.id.navigationBarBackground, true)
         }
-
         window.enterTransition = transition
         //!! Transition sin terminar para el detail
 
-        //ACA ES DONDE HAY QUE ARREGLAR QUE TRAIGA EL PRODUCTO CLICKEADO
+        //Se realiza el binding de la informacion recibida desde el lsit fragement  al detail fragment
+
         val id = intent.getIntExtra("id", 0)
         var product=products[id]
         val detailFragment = supportFragmentManager.findFragmentById(R.id.fragmentDetail) as? DetailFragment
         if (product != null) {
             detailFragment?.showProduct(product)
-/*
-        val product = intent.getParcelableExtra<Product>(PRODUCT)
-        val detailFragment = supportFragmentManager.findFragmentById(R.id.fragmentDetail) as? DetailFragment
-        if (product != null) {
-            detailFragment?.showProduct(product)
 
-        }*/
 
         //Al seleccionar el boton de "Agregar al Carrito " se redirecciona al Activity de Shop, ingresando al fragmente de Carrito
-        // Te redirecciona porque le pasamos como valor de origen "DETAIL"
         addBoton = findViewById(R.id.addCarrito)
         addBoton.setOnClickListener{
                 runBlocking{
                     animarCarrito(carroCompra,R.raw.carrito)
                     delay(200)
                 }
-            val intent= Intent(this, Shop::class.java).apply {  }
 
+            val intent= Intent(this, Shop::class.java).apply {
             //DATOS EXTRA
-            intent.putExtra("origen", "DETAIL");
+            intent.putExtra("origen", "DETAIL")
+            putExtra(Intent.EXTRA_INDEX, product.id)
+            };
 
             startActivity(intent)
+
 
         }
 
