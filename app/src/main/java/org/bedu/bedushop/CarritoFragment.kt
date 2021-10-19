9fc431df946bf7eeb5681fa239093890e556ba4e
PabDelCanto <56207062+PabDelCanto@users.noreply.github.com>
2021-10-19 01:11:05 -0300

    Carrito pasa item pero no hay persistencia

--- app/src/main/java/org/bedu/bedushop/CarritoFragment.kt
+++ app/src/main/java/org/bedu/bedushop/CarritoFragment.kt
@@ -27,7 +27,8 @@ class CarritoFragment : Fragment() {
     private lateinit var mpPrice: TextView
 
     //Esta es la lista de Productos que tenemos real en el carrito, por sus id
-    private var products: MutableList<Int> = mutableListOf()
+    private var products : MutableList<ProductoApi> = mutableListOf()
+    private var idProducts : MutableList<Int> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,9 +38,6 @@ class CarritoFragment : Fragment() {
         // Inflate the layout for this fragment
         val resumen = ResumenPagoFragment()
         val view = inflater.inflate(R.layout.fragment_carrito, container, false)
-
-        val bundleDesdeDetail = arguments?.getParcelable<ProductoApi>(SHOP_PRODUCT)
-        Log.d("Producto en carrito", bundleDesdeDetail?.id.toString())
 
         //Funcion para pasar al fragment de compra
         val boton = view.findViewById<Button>(R.id.buttonComprar)
@@ -52,48 +50,22 @@ class CarritoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-
-
-//Traigo como argumento el id del elemento que se agrego al carrito
-        val ids= arguments?.getInt("idProd")
-        Log.d("argument", ids.toString())
+        val bundleDesdeDetail = arguments?.getParcelable<ProductoApi>(SHOP_PRODUCT)!! //No hay persistencia en el array!
 
 //Si no fallo mim bundle, y por lo tanto no es nulo
-            if(ids!=null){
-                if ((products.find{it==ids})!=null){
+            if(bundleDesdeDetail.id!=null){
+                if ((products.find{it.id in idProducts})!=null){
+                    products[id].stock++
                     Log.d("cantidad","cambiar")
+                    Log.d("Stock", products[id].stock.toString())
                 }
                 else{
-                    products.add(ids)
+                    products.add(bundleDesdeDetail)
+                    idProducts.add(bundleDesdeDetail.id)
                     Log.d("crear","itemnuevo")
                 }
-                    setUpRecyclerView()
-}
-
-
-
-/*Si no fallo mim bundle, y por lo tanto no es nulo
-if(ids!=null){
-//Veo si ese elemento ya existe en mi carrito
-if ((products.find{it==ids})!=null){
-    //llamo al producto con dicho id y aumento su cantidad en 1
-
-val productoBuscado=bd.find{id==ids}
-    Log.d("buscado",productoBuscado.toString())
-
-// productoBuscado.cantidad=++
-
-}
-else{
-    //Aca hay que modificar el recyler view para que cree el elemento a partir del id del producto
-    setUpRecyclerView1(id)
-    //agrego el id en el producto para que no se cree dos veces
-    products.add(id)
-}
-
-}*/
-
+                    setUpRecyclerView(products)
+            }
 }
 
 private fun replaceFragment(fragment: Fragment, bundle: Bundle?) {
@@ -105,33 +77,7 @@ trans.commit()
 }
 
 
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-/*val realm = Realm.getDefaultInstance()
-products = realm.where(Product::class.java).findAll()
-val product = intent.getIntExtra("id", 0)
-var product = products[id].id
-val marketObject =
-    supportFragmentManager.findFragmentById(R.id.recyclerCarrito) as? CarritoFragment
-if (product != null) {
-    marketObject?.showInMarket(product, products)
-}*/
-}
-
-
-//funcion en proceso para linkear elementos on lo visual
-
-fun showInMarket(id: Int, product: List<Product>) {
-    val producto=product[id]
-    view?.visibility = View.VISIBLE
-    mpName.text = producto.name
-   Picasso.with(requireContext()).load(producto.idImage).into(mpimg)
-   mpPrice.text = "$${producto.price}"
-
-}
-
-
-private fun setUpRecyclerView() {
+private fun setUpRecyclerView(listadoProductosCarrito : MutableList<ProductoApi>) {
 var listStr = this.arguments?.getString(MainApp.array)
 val listProductType = object : TypeToken<MutableList<ProductoApi>>(){}.type
 val prods = Gson().fromJson<MutableList<ProductoApi>>(listStr, listProductType)
@@ -139,10 +85,12 @@ val prods = Gson().fromJson<MutableList<ProductoApi>>(listStr, listProductType)
 recyclerCarrito.setHasFixedSize(true)
 recyclerCarrito.layoutManager = LinearLayoutManager(activity)
 //seteando el Adapter
-val testProd : MutableList<ProductoApi> = mutableListOf()
+/*val testProd : MutableList<ProductoApi> = mutableListOf()
 testProd.add(ProductoApi(1,"Mochila", 21.50f, "Mochila test", rating(4.10,50), "https://http2.mlstatic.com/D_NQ_NP_746908-MLA46933276297_072021-O.webp"))
 testProd.add(ProductoApi(2,"Mochila", 21.50f, "Mochila test", rating(4.10,50), "https://http2.mlstatic.com/D_NQ_NP_746908-MLA46933276297_072021-O.webp"))
-mAdapter = RecyclerAdapterCarrito(requireActivity(), testProd)//esto es uma prueba
+*/
+
+mAdapter = RecyclerAdapterCarrito(requireActivity(), listadoProductosCarrito)//esto es uma prueba
 //asignando el Adapter al RecyclerView
 recyclerCarrito.adapter = mAdapter
 }
